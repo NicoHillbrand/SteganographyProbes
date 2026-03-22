d2a214a5d33776e14ab23f83f92f3fd4a52a6d52
Sweta <swetajena98@gmail.com>
2026-03-22 14:25:32 +0530

    adding config file

---
+++ steer/config.py
@@ -0,0 +1,10 @@
+config={}
+config['CACHE_DIR']= '/scratch/sweta.j'
+config['DEVICE'] = 'cuda'
+# model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+config['MODEL_ID'] = "Qwen/Qwen3-1.7B"
+config['DATA_PATH'] = "/home/sweta.j/steg/SteganographyProbes/data/game_scenarios_v2.jsonl"
+
+
+config['NUM_SAMPLES'] = -1
+config['K_PCT'] = 10
